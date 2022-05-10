9ddb965d9a943a7f7c5144bcceb06889a83f5081
Dustin <dusmer@gmail.com>
2022-05-09 22:11:50 -0700

    Added game state functionality Can not continue game once score is reached

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-function test(){
-    console.log("It worked");
-}
+//Gloabl score keeping varibles
 
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+let gameStatus = 0
+
+//Use a random number generator to pick and return a random option from the array
 function computerPlay(){
-    //returns random rock paper or scissors
+
     const options = ["rock", "paper", "scissors"];
     let compPlay = options[Math.floor(Math.random()*options.length)];
     return compPlay;
 }
 
+//Determine a winner based on the player and computer selection
 function playRound(playerSelection,computerSelection){
 
     if (playerSelection.toLowerCase() == "rock"){
@@ -60,6 +65,8 @@ function playRound(playerSelection,computerSelection){
     }
 }
 
+
+
 function playGame(playerSelection){
     const computerSelection = computerPlay();
     const result = playRound(playerSelection,computerSelection);
@@ -68,35 +75,85 @@ function playGame(playerSelection){
 
 }
 
+//Calling game() restarts the game, and sets the scores to 0
 function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let tieScore = 0;
+    playerScore = 0;
+    computerScore = 0;
+    tieScore = 0;
+    gameStatus = 1;
+
+    document.getElementById("paper").innerHTML = "<a href=\"javascript:playerSelect('paper');\"><img src=\"images/paper.png\" /></a>";
+
+    document.getElementById("rock").innerHTML = "<a href=\"javascript:playerSelect('rock');\"><img src=\"images/rock.png\" /></a>";
+
+    document.getElementById("scissors").innerHTML = "<a href=\"javascript:playerSelect('scissors');\"><img src=\"images/scissors.png\" /></a>";
   
-    for (let i = 0; i < 5; i++){
-        playerSelection = prompt("Rock, paper, or scissors?");
-        const computerSelection = computerPlay();
-        const result = playRound(playerSelection,computerSelection);
-        console.log(`Player:${playerSelection} \n Computer: ${computerSelection}`)
-        console.log(result);
-        switch(result){
-            case "You Win!":
-            playerScore++;
-            document.getElementById("playerScoreDisplay").textContent = playerScore;
-            
-            break;
+    document.getElementById("playerScoreDisplay").innerHTML = playerScore;
+    document.getElementById("computerScoreDisplay").innerHTML = computerScore;
+    document.getElementById("tieScoreDisplay").innerHTML = tieScore;
 
-            case "You Lose!":
-            computerScore++;
-            document.getElementById("computerScoreDisplay").innerHTML = computerScore;
-            break;
+}
 
-            case "You Tie!":
-            tieScore++;
-            document.getElementById("tieScoreDisplay").innerHTML = tieScore;
-            break;
-        }
-        console.log(`Score is ${playerScore} - ${computerScore} - ${tieScore}`);
+//Check if either player score or computer score has reached 5
+function gameOver(){
+    if (playerScore == 5 || computerScore == 5){
+        document.getElementById("paper").innerHTML = "Paper";
+        document.getElementById("rock").innerHTML = "Rock";
+        document.getElementById("scissors").innerHTML = "Scissors";
+        
+        return (playerScore == 5 ? 1 : 2)
 
     }
+    else{
+        return 0;
+    }
+}
+
+function playerSelect(choice){
+
+
+    const computerSelection = computerPlay();
+    const playerSelection = choice;
+
+    //Display computer and player selections
+    const playerSelectionImage = `<img src=\"images/${playerSelection}.png\" /><br />${playerSelection}`;
+    const computerSelectionImage = `<img src=\"images/${computerSelection}.png\" />`;
+    console.log(playerSelectionImage);
+
+    
+    document.getElementById("player").innerHTML = playerSelectionImage;
+    document.getElementById("computer").innerHTML = computerSelectionImage;
+    
+    //Get round winner based on selections and display winner message
+    const result = playRound(playerSelection,computerSelection);
+    document.getElementById("roundStatus").innerHTML = result;
+    switch(result){
+        case "You Win!":
+        playerScore++;
+        document.getElementById("playerScoreDisplay").innerHTML = playerScore;
+        break;
+
+        case "You Lose!":
+        computerScore++;
+        document.getElementById("computerScoreDisplay").innerHTML = computerScore;
+        break;
+
+        case "You Tie!":
+        tieScore++;
+        document.getElementById("tieScoreDisplay").innerHTML = tieScore;
+        break;
+    }
+
+    //Check if the game is over and display winner if it is
+    let status = gameOver();
+    if (status == 1){
+        alert("Game over you win");
+        gameStatus = 0;
+    }
+    else if (status == 2){
+        alert("Game over you lose *wump womp*");
+        gameStatus = 0;
+    }
+
+
 }
